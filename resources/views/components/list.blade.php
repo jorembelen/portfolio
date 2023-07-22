1c5c7de88e7d1c763ccd705fde2921e831900f2b
jorem belen <jorembelen@gmail.com>
2023-07-22 13:53:26 +0300

    wip

---
+++ resources/views/components/list.blade.php
@@ -0,0 +1,12 @@
+
+<ul class="portfolio-categories">
+    <li>
+        <a href="#">Laravel</a>
+    </li>
+    <li>
+        <a href="#">Livewire</a>
+    </li>
+    <li>
+        <a href="#">Bootstrap</a>
+    </li>
+</ul>
